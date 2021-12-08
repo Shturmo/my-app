5a7404b36c0a27a6e42f51d78967bad287a87610
Ilya Ivanenko <ilya__ivanenko@mail.ru>
2021-12-08 18:46:13 +0300

    state of project after theory 3 lessons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,57 +1,46 @@
 import React, { useState } from 'react'
 
-const Counter = () => {
-  const [count, setCount] = useState(0)
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
+const Counter = (props) => {
+  const { value } = props
+  // Т.к. объект управляемый у него не дожно быть useState
+  // const [value, setValue] = useState(props.value)
 
-  const formatCount = () => {
-    return count === 0 ? 'empty' : count
+  const formatValue = () => {
+    return value === 0 ? 'empty' : value
   }
   const getBageClasses = () => {
     let classes = 'badge m-2 '
-    return (classes += count === 0 ? 'bg-warning' : 'bg-primary')
+    return (classes += value === 0 ? 'bg-warning' : 'bg-primary')
   }
 
   const handleIncrement = () => {
-    setCount((prevState) => prevState + 1)
-    console.log(count)
+    console.log('handle increment')
+    // setValue((prevState) => prevState + 1)
   }
   const handleDecrement = () => {
-    if (count > 0) {
-      setCount((prevState) => prevState - 1)
-    }
-    console.log(count)
-  }
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id))
-  }
-  const renderTags = () => {
-    return tags.length !== 0
-      ? tags.map((tag) => (
-          <li
-            key={tag}
-            className="btn btn-primary btn-sm m-2"
-            onClick={() => handleTagChange(tag)}
-          >
-            {tag}
-          </li>
-        ))
-      : 'No tags'
+    console.log('handle decrement')
+    // if (value > 0) {
+    //   setValue((prevState) => prevState - 1)
+    // }
   }
 
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>
-  }
   return (
-    <>
-      <span className={getBageClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
   )
 }
 
